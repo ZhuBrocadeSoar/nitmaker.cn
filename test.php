4710f2d33d78b6f963f9624af5db73797a31ffcc
Zhu Brocade Soar <soar@localhost.localdomain>
2016-10-29 14:32:47 +0800

    test the mysql connect

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php
+$con = mysql_connect("123.206.204.23", "nitmaker_cn", "nitmaker.cn");
+if(!$con){
+    die("Could not connect to mysql: " . mysql_error());
+}else{
+    echo "<p>Successfully connected</p>";
+}
 phpinfo();
 ?>
