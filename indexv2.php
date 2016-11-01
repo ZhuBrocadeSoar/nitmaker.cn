3d0a6e620626044e2d6d3957e871e0661a7ea81d
Zhu Brocade Soar <soar@localhost.localdomain>
2016-11-01 19:15:08 +0800

    update indexv2.php

--- indexv2.php
+++ indexv2.php
@@ -1,51 +1,55 @@
 <?php
-    session_start();
+session_start();
+
+//检查是否执行登录操作
+if($_SESSION['logInState'] != "logIned"){
     //建立持久的数据库连接
     $_SESSION['conOfMysql'] = mysql_pconnect("localhost", "nitmaker_cn", "nitmaker.cn");
     //////////////////////////////////////////数据库地址///数据库名///////用户名///////
 
 
-//判断表单提交次数
-if($_SESSION['submitCount'] >= 1 && $_SESSION['logInState'] != "logIned"){
-    //提交数已经记录，发生过至少一次提交。
-    $_SESSION['submitCount']++;
-    //接受提交的表单并安全化
-    $_SESSION['nickName'] = htmlspecialchars(stripslashes(trim($_POST['nickName'])));
-    $_SESSION['passwd'] = htmlspecialchars(stripslashes(trim($_POST['passwd'])));
-    //密码转换为SHA1值
-    $_SESSION['passwd'] = sha1($_SESSION['passwd']);
-    //匹配nickName和passwd
-    //选择数据库
-    mysql_select_db("nitmaker_cn", $_SESSION['conOfMysql']);
-    /////////////////数据库名////
-    //转储字符串
-    $tmp = $_SESSION['nickName'];
-    //查询数据库匹配nickName, 转义字符\"很关键
-    $result = mysql_query("SELECT id,nickName,passwd FROM userInfo WHERE nickName = \"$tmp\"", $_SESSION['conOfMysql']);
-    if($row = mysql_fetch_array($result)){
-        //匹配到nickName
-        if($row['passwd'] == $_SESSION['passwd']){
-            //密码匹配
-            $_SESSION['logInInfoState'] = "allMached";
+    //判断表单提交次数
+    if($_SESSION['submitCount'] >= 1){
+        //提交数已经记录，发生过至少一次提交。
+        $_SESSION['submitCount']++;
+        //接受提交的表单并安全化
+        $_SESSION['nickName'] = htmlspecialchars(stripslashes(trim($_POST['nickName'])));
+        $_SESSION['passwd'] = htmlspecialchars(stripslashes(trim($_POST['passwd'])));
+        //密码转换为SHA1值
+        $_SESSION['passwd'] = sha1($_SESSION['passwd']);
+        //匹配nickName和passwd
+        //选择数据库
+        mysql_select_db("nitmaker_cn", $_SESSION['conOfMysql']);
+        /////////////////数据库名////
+        //转储字符串
+        $tmp = $_SESSION['nickName'];
+        //查询数据库匹配nickName, 转义字符\"很关键
+        $result = mysql_query("SELECT id,nickName,passwd FROM userInfo WHERE nickName = \"$tmp\"", $_SESSION['conOfMysql']);
+        if($row = mysql_fetch_array($result)){
+            //匹配到nickName
+            if($row['passwd'] == $_SESSION['passwd']){
+                //密码匹配
+                $_SESSION['logInInfoState'] = "allMached";
+            }else{
+                //密码不匹配
+                $_SESSION['logInInfoState'] = "passwdNotMach";
+            }
         }else{
-            //密码不匹配
-            $_SESSION['logInInfoState'] = "passwdNotMach";
+            //昵称不匹配
+            $_SESSION['logInInfoState'] = "nickNameNotMach";
         }
     }else{
-        //昵称不匹配
-        $_SESSION['logInInfoState'] = "nickNameNotMach";
+        //未提交任何表单，判定为第一次进入该站点
+        //记录访问时间;
+        $_SESSION['accessTime'] = time();
+        //第一次访问设定为第1次提交表单;
+        $_SESSION['submitCount'] = 1;
+        //初始化的登录信息
+        $_SESSION['nickName'] = "nick_name";
+        $_SESSION['passwd'] = "password";
+        $_SESSION['logInState'] = "notLogIn";
+        $_SESSION['logInInfoState'] = "noInput";
     }
-}else{
-    //未提交任何表单，判定为第一次进入该站点
-    //记录访问时间;
-    $_SESSION['accessTime'] = time();
-    //第一次访问设定为第1次提交表单;
-    $_SESSION['submitCount'] = 1;
-    //初始化的登录信息
-    $_SESSION['nickName'] = "nick_name";
-    $_SESSION['passwd'] = "password";
-    $_SESSION['logInState'] = "notLogIn";
-    $_SESSION['logInInfoState'] = "noInput";
 }
 ?>
 <html>
