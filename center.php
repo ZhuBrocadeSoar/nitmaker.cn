3e5365e73bdce58e3518f5d324c675ce31ef9ff7
Zhu Brocade Soar <soar@localhost.localdomain>
2016-11-01 19:54:04 +0800

    first function is view user info

--- center.php
+++ center.php
@@ -24,10 +24,14 @@ if($_SESSION['logInState'] != "logIned"){
     //尚未登录，打印转跳信息
     echo "<p>请登录，将在2秒后转跳到登录界面</p>";
 }else{
-    echo "<h1>Here is user\'s Center</h1>";
+    echo "<h1>This is user's Center</h1>";
     $tmp = $_SESSION['nickName'];
-    echo "<p>Hello \"$tmp\"</p>";
+    echo "<p>Hello $tmp</p>";
     echo "<p>Pick a function:</p>";
+    echo "<table border = 1>";
+    $tmp = "123.206.204.23/nitmaker.cn/userinfo.php";
+    echo "<tr><th>个人信息</th><td> <form method = \"post\" action = \"$tmp\"><input type = \"submit\" value = \"点击查看\"></form> <td></tr>";
+    echo "</table>";
 }
 ?>
 
