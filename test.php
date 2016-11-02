f074dc8a051c9c7b12b6d688ed2a6eb059cc9df3
Zhu Brocade Soar <soar@localhost.localdomain>
2016-11-02 19:53:33 +0800

    test class include

--- test.php
+++ test.php
@@ -1,16 +1,3 @@
 <?php
-$con = mysql_connect("localhost", "nitmaker_cn", "nitmaker.cn");
-if(!$con){
-    die("Could not connect to mysql: " . mysql_error() . "contast with zhujinteng2012@163.com");
-}else{
-    echo "<p>Successfully connected</p>";
-}
-mysql_select_db("nitmaker_cn");
-$result = mysql_query("SELECT * FROM userInfo");
-$row = mysql_fetch_array($result);
-foreach($row as $key=>$value){
-    echo $key . "    " . $value;
-    echo "<br/>";
-}
-phpinfo();
+require('/root/Downloads/SMTPmail_class.php');
 ?>
