da010edf76e1e429e07c780806307e8b35f920da
Zhu Brocade Soar <soar@localhost.localdomain>
2016-11-17 14:37:03 +0800

    a reborn site

---
+++ reborn/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>欢迎来到NITmaker</title>
+</head>
+<body>
+<h1>关于本站点</h1><br />
+<p>本站点是NITmaker的一个线上服务站点，目前提供的主要功能是Shadowsocks福利资源的申请、发放、查询</p>
+<h1>
+<a href = "<?php
+echo htmlspecialchars(dirname(__FILE__) . "signup.php");
+?>">注册</a>或者<a href = "<?php
+echo htmlspecialchars(dirname(__FILE__) . "login.php");
+?>">登录</a>
+</h1>
+</body>
+</html>
