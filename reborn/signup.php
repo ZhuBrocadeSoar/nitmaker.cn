53f45b7fba07e9944195888925f7365d69e3a0e6
soar <zhujinteng2012@hotmail.com>
2017-01-19 22:05:38 +0800

    resister page

--- reborn/signup.php
+++ reborn/signup.php
@@ -7,16 +7,17 @@ if(isset($_SESSION['loginStatus'])){//释放原有登录会话
 
 <html>
 	<head>
-		<title>注册</title>
+		<title>NITmaker-注册</title>
 	</head>
 
 	<body>
 		<h1>注册</h1>
 		<form method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
 			<table border = 1>
-				<tr> <th>用户名</th> <td><input type = 'text' name = 'userTypedUserName' value = <?php echo $_SESSION['userTypedUserName']?>></td> </tr>
-				<tr> <th>密码</th> <td><input type = 'password' name = 'userTypedPassword' value = <?php echo $_SESSION['userTypedPassword']?>></td> </tr>
-				<tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo $_SESSION['userTypedVerifCode']?>></td> </tr>
+				<tr> <th>用户名</th> <td><input type = 'text' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\''?>></td> </tr>
+				<tr> <th>密码</th> <td><input type = 'password' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['userTypedPassword'].'\''?>></td> </tr>
+				<tr> <th>邮箱</th> <td><input type = 'text' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\''?>></td> </tr>
+				<tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo '\''.$_SESSION['userTypedVerifCode'].'\''?>></td> </tr>
 			</table>
 			<img title = '点击刷新' src = '../src/verification_class.php' align = 'absbottom' onclick = "this.src='../src/verification_class.php?'+Math.random();"></img>
 			<input type = 'submit' value = '注册'>
