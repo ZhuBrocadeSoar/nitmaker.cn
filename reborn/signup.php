93c728e1cccbf15b1e6afb0a8bada0d49340784c
soar <zhujinteng2012@hotmail.com>
2017-01-21 01:12:16 +0800

    resister page

--- reborn/signup.php
+++ reborn/signup.php
@@ -24,11 +24,18 @@ if(isset($_POST)){
 			document.getElementById('ruleOfUserNameObjuct').innerHTML = '';
 		}
 	}
-	function ruleOfPassword(){
+	function ruleOfPassword(operate){
 		if(operate == 1){
 			document.getElementById('ruleOfPasswordObjuct').innerHTML = '密码可包含字母(区分大小写)、数字或标点符号，不能有空格等空白符，长度为6~14个字符';
 		}else if(operate == 0){
 			document.getElementById('ruleOfPasswordObjuct').innerHTML = '';
+		}
+	}
+	function ruleOfEmail(operate){
+		if(operate == 1){
+			document.getElementById('ruleOfEmailObjuct').innerHTML = '填写一个有效的电子邮箱地址，用于接收验证码';
+		}else if(operate == 0){
+			document.getElementById('ruleOfEmailObjuct').innerHTML = '';
 		}
 	}
 </script>
@@ -44,7 +51,7 @@ if(isset($_POST)){
 			<table border = 0>
 				<tr> <th>用户名</th> <td><input type = 'text' id = 'usernameObjuct' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\'';?> onfocus = "ruleOfUserName(1)" onblur = "ruleOfUserName(0)"></td> <td id = 'ruleOfUserNameObjuct'></td> </tr>
 				<tr> <th>密码</th> <td><input type = 'password' id = 'passwordObjuct' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['modifiedUserTypedPassword'].'\'';?> onclick = "emptyPassword(1)" onfocus = "ruleOfPassword(0)"></td> <td id = 'ruleOfPasswordObjuct'></td> </tr>
-				<tr> <th>邮箱</th> <td><input type = 'text' id = 'emailObjuct' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\'';?> onfocus = "ruleOfEmail()"></td> </tr>
+				<tr> <th>邮箱</th> <td><input type = 'text' id = 'emailObjuct' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\'';?> onfocus = "ruleOfEmail(1)" onblur = "ruleOfEmail(0)"></td> <td id = 'ruleOfEmailObjuct'></td> </tr>
 				<tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo '\''.$_SESSION['userTypedVerifCode'].'\'';?>></td> </tr>
 			</table>
 			<img title = '点击刷新' src = '../src/verification_class.php' align = 'absbottom' onclick = "this.src='../src/verification_class.php?'+Math.random();"></img>
