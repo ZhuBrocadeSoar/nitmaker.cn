876c769737cd62348325a3976a522856f45361ca
soar <zhujinteng2012@hotmail.com>
2017-01-21 01:06:30 +0800

    resister page

--- reborn/signup.php
+++ reborn/signup.php
@@ -16,11 +16,20 @@ if(isset($_POST)){
 	function emptyPassword(){
 		document.getElementById('passwordObjuct').value = '';
 	}
-	function ruleOfUserName(){
-		document.getElementById('ruleOfUserNameObjuct').innerHTML = '用户名由字母(区分大小写)、数字或者下划线组成，最多32个字符，用于用户登录';
+	function ruleOfUserName(operate){
+		if(operate == 1){
+			document.getElementById('ruleOfUserNameObjuct').innerHTML = '用户名由字母(区分大小写)、数字或者下划线组成，最多32个字符，用于用户登录';
+		}
+		else if(operate == 0){
+			document.getElementById('ruleOfUserNameObjuct').innerHTML = '';
+		}
 	}
 	function ruleOfPassword(){
-		document.getElementById('ruleOfPasswordObjuct').innerHTML = '密码可包含字母(区分大小写)、数字或标点符号，不能有空格等空白符，长度为6~14个字符';
+		if(operate == 1){
+			document.getElementById('ruleOfPasswordObjuct').innerHTML = '密码可包含字母(区分大小写)、数字或标点符号，不能有空格等空白符，长度为6~14个字符';
+		}else if(operate == 0){
+			document.getElementById('ruleOfPasswordObjuct').innerHTML = '';
+		}
 	}
 </script>
 
@@ -33,8 +42,8 @@ if(isset($_POST)){
 		<h1>注册</h1>
 		<form method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
 			<table border = 0>
-				<tr> <th>用户名</th> <td><input type = 'text' id = 'usernameObjuct' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\'';?> onfocus = "ruleOfUserName()"></td> <td id = 'ruleOfUserNameObjuct'></td> </tr>
-				<tr> <th>密码</th> <td><input type = 'password' id = 'passwordObjuct' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['modifiedUserTypedPassword'].'\'';?> onclick = "emptyPassword()" onfocus = "ruleOfPassword()"></td> <td id = 'ruleOfPasswordObjuct'></td> </tr>
+				<tr> <th>用户名</th> <td><input type = 'text' id = 'usernameObjuct' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\'';?> onfocus = "ruleOfUserName(1)" onblur = "ruleOfUserName(0)"></td> <td id = 'ruleOfUserNameObjuct'></td> </tr>
+				<tr> <th>密码</th> <td><input type = 'password' id = 'passwordObjuct' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['modifiedUserTypedPassword'].'\'';?> onclick = "emptyPassword(1)" onfocus = "ruleOfPassword(0)"></td> <td id = 'ruleOfPasswordObjuct'></td> </tr>
 				<tr> <th>邮箱</th> <td><input type = 'text' id = 'emailObjuct' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\'';?> onfocus = "ruleOfEmail()"></td> </tr>
 				<tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo '\''.$_SESSION['userTypedVerifCode'].'\'';?>></td> </tr>
 			</table>
