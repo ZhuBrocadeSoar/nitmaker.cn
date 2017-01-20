29b6caa4f22e1b91f8d8ff8ed8f41e463990068f
soar <zhujinteng2012@hotmail.com>
2017-01-20 22:47:50 +0800

    resister page

--- reborn/signup.php
+++ reborn/signup.php
@@ -6,15 +6,21 @@ if(isset($_SESSION['loginStatus'])){//释放原有登录会话
 if(isset($_POST)){
 	$_SESSION['userTypedUserName'] = htmlspecialchars($_POST['userTypedUserName']);
 	$_SESSION['userTypedPassword'] = htmlspecialchars($_POST['userTypedPassword']);
-	parse_str(sha1($_SESSION['userTypedPassword'], &$sha1Of);
-	for($i = 0; $i < strlen($_POST[userTypedPassword]); $i ++){
-		$_SESSION['modifiedUserTypedPassword'][$i] = $sha1Of[$i];
-	}
-	//$_SESSION['modifiedUserTypedPassword'][$i] = '\0';
 	$_SESSION['userTypedEmail'] = htmlspecialchars($_POST['userTypedEmail']);
 	$_SESSION['userTypedVerifCode'] = htmlspecialchars($_POST['userTypedVerifCode']);
 }
 ?>
+
+<script language = 'php'>
+	var x = document.getElementById('signupinfo');
+	function changeValueOfPassword($status){
+		if($status == 'currect'){
+			x.elements[1].value = $_SESSION['userTypedPassword'];
+		}else if($status == 'sha1value'){
+			x.elements[1].value = sha1($_SESSION['userTypedPassword']);
+		}
+	}
+</script>
 
 <html>
 	<head>
@@ -23,10 +29,10 @@ if(isset($_POST)){
 
 	<body>
 		<h1>注册</h1>
-		<form method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
+		<form id = 'signupinfo' method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
 			<table border = 1>
 				<tr> <th>用户名</th> <td><input type = 'text' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\'';?>></td> </tr>
-				<tr> <th>密码</th> <td><input type = 'password' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['modifiedUserTypedPassword'].'\'';?>></td> </tr>
+				<tr> <th>密码</th> <td><input type = 'password' name = 'userTypedPassword' onfocus = "changeValueOfPassword('curect')" onblur = "changeValueOfPassword('sha1value')"></td> </tr>
 				<tr> <th>邮箱</th> <td><input type = 'text' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\'';?>></td> </tr>
 				<tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo '\''.$_SESSION['userTypedVerifCode'].'\'';?>></td> </tr>
 			</table>
