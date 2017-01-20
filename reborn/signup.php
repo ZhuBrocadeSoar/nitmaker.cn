8d9e4fa8904dace44fcb7bc40cf9b6403de79b8c
soar <zhujinteng2012@hotmail.com>
2017-01-20 23:57:31 +0800

    resister page

--- reborn/signup.php
+++ reborn/signup.php
@@ -6,6 +6,7 @@ if(isset($_SESSION['loginStatus'])){//释放原有登录会话
 if(isset($_POST)){
 	$_SESSION['userTypedUserName'] = htmlspecialchars($_POST['userTypedUserName']);
 	$_SESSION['userTypedPassword'] = htmlspecialchars($_POST['userTypedPassword']);
+	$_SESSION['modifiedUserTypedPassword'] = substr(sha1($_SESSION['userTypedPassword']), 0, strlen($_SESSION['userTypedPassword']) - 1);
 	$_SESSION['userTypedEmail'] = htmlspecialchars($_POST['userTypedEmail']);
 	$_SESSION['userTypedVerifCode'] = htmlspecialchars($_POST['userTypedVerifCode']);
 }
@@ -27,7 +28,7 @@ if(isset($_POST)){
 		<form method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
 			<table border = 1>
 				<tr> <th>用户名</th> <td><input type = 'text' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\'';?>></td> </tr>
-				<tr> <th>密码</th> <td><input type = 'password' id = 'passwordObjuct' name = 'userTypedPassword' value = <?php echo '\''.sha1($_SESSION['userTypedPassword']).'\'';?> onclick = "emptyPassword()"></td> </tr>
+				<tr> <th>密码</th> <td><input type = 'password' id = 'passwordObjuct' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['userTypedPassword'].'\'';?> onclick = "emptyPassword()"></td> </tr>
 				<tr> <th>邮箱</th> <td><input type = 'text' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\'';?>></td> </tr>
 				<tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo '\''.$_SESSION['userTypedVerifCode'].'\'';?>></td> </tr>
 			</table>
