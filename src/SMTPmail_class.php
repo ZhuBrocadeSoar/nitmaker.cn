553d807de331fd0f392465273fa80a8381e32a97
soar <zhujinteng2012@hotmail.com>
2017-01-21 16:34:37 +0800

    resister page

--- src/SMTPmail_class.php
+++ src/SMTPmail_class.php
@@ -467,8 +467,8 @@ class MySendMail {
 }
 ///**************************** Test ***********************************/
 $mail = new MySendMail();
-$mail->setServer("smtp.163.com", "zhujinteng2012@163.com", "NO.10823");
-$mail->setFrom("zhujinteng2012");
+$mail->setServer("smtp.163.com", "nitmaker@163.com", "nitmaker0soar");
+$mail->setFrom("nitmaker@163.com");
 $mail->setReceiver("1422090554@qq.com");
 //////$mail->setReceiver("XXXXX@XXXXX");
 ////$mail->setCc("XXXXX@XXXXX");
@@ -476,6 +476,6 @@ $mail->setReceiver("1422090554@qq.com");
 ////$mail->setBcc("XXXXX@XXXXX");
 ////$mail->setBcc("XXXXX@XXXXX");
 ////$mail->setBcc("XXXXX@XXXXX");
-$mail->setMailInfo("NITmaker", "<b>NITmaker</b>");
+$mail->setMailInfo("something", "<b>remember you can not set this file in www</b>");
 $mail->sendMail();
 ?>
