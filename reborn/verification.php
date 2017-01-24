78c2e4ab0592e628ba9bd577c98499bc8611212a
soar <zhujinteng2012@hotmail.com>
2017-01-24 11:20:03 +0800

    verification page

--- reborn/verification.php
+++ reborn/verification.php
@@ -2,15 +2,7 @@
 session_start();
 
 if(!isset($_SESSION['verifQuery'])){
-	echo "<html>
-	<head>
-		<title></title>
-		<meta http-equiv = \'refresh\' content = \'0;url = http://123.206.204.23/nitmaker.cn/reborn/\'>
-	</head>
-	<body>
-	
-	</body>
-	</html>";
+	die("Permition Denied!");
 }else{
 	echo "hello";
 	unset($_SESSION['verifQuery']);
