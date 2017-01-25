edaf2fbf7971629ec175983c933a8602e4ca8ecb
Zhu Brocade Soar <soar@localhost.localdomain>
2017-01-25 09:58:27 +0800

    the first time for signup

--- reborn/signup.php
+++ reborn/signup.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['signupTrying'])){//释放原有登录会话
-	session_unset();
+    session_unset();
 }
 
 $_SESSION['signupTrying'] = '1';
@@ -9,89 +9,90 @@ $_SESSION['conOfMysql'] = mysql_pconnect("localhost", "nitmaker_cn", "nitmaker.c
 mysql_select_db("nitmaker_cn", $_SESSION['conOfMysql']);
 
 if(isset($_POST['submitStatus'])){
-	$_SESSION['userTypedUserName'] = htmlspecialchars($_POST['userTypedUserName']);
-	$_SESSION['userTypedPassword'] = htmlspecialchars($_POST['userTypedPassword']);
-	$_SESSION['modifiedUserTypedPassword'] = substr(sha1($_SESSION['userTypedPassword']), 0, strlen($_SESSION['userTypedPassword']));
-	$_SESSION['userTypedEmail'] = htmlspecialchars($_POST['userTypedEmail']);
-	$_SESSION['userTypedVerifCode'] = htmlspecialchars($_POST['userTypedVerifCode']);
+    $_SESSION['userTypedUserName'] = htmlspecialchars($_POST['userTypedUserName']);
+    $_SESSION['userTypedPassword'] = htmlspecialchars($_POST['userTypedPassword']);
+    $_SESSION['modifiedUserTypedPassword'] = substr(sha1($_SESSION['userTypedPassword']), 0, strlen($_SESSION['userTypedPassword']));
+    $_SESSION['userTypedEmail'] = htmlspecialchars($_POST['userTypedEmail']);
+    $_SESSION['userTypedVerifCode'] = htmlspecialchars($_POST['userTypedVerifCode']);
 }
 ?>
 
 <script>
-	function emptyPassword(){
-		document.getElementById('passwordObjuct').value = '';
-	}
-	function ruleOfUserName(operate){
-		if(operate == 1){
-			document.getElementById('ruleOfUserNameObjuct').innerHTML = '用户名由字母(区分大小写)、数字或者下划线组成，最多32个字符，用于用户登录';
-		}
-		else if(operate == 0){
-			document.getElementById('ruleOfUserNameObjuct').innerHTML = '';
-		}
-	}
-	function ruleOfPassword(operate){
-		if(operate == 1){
-			emptyPassword();
-			document.getElementById('ruleOfPasswordObjuct').innerHTML = '密码可包含字母(区分大小写)、数字或英文标点符号，不能有空格等空白符，长度为6~14个字符';
-		}else if(operate == 0){
-			document.getElementById('ruleOfPasswordObjuct').innerHTML = '';
-		}
-	}
-	function ruleOfEmail(operate){
-		if(operate == 1){
-			document.getElementById('ruleOfEmailObjuct').innerHTML = '填写一个有效的电子邮箱地址，用于接收验证码';
-		}else if(operate == 0){
-			document.getElementById('ruleOfEmailObjuct').innerHTML = '';
-		}
-	}
+function emptyPassword(){
+    document.getElementById('passwordObjuct').value = '';
+    }
+    function ruleOfUserName(operate){
+        if(operate == 1){
+            document.getElementById('ruleOfUserNameObjuct').innerHTML = '用户名由字母(区分大小写)、数字或者下划线组成，最多32个字符，用于用户登录';
+        }
+        else if(operate == 0){
+            document.getElementById('ruleOfUserNameObjuct').innerHTML = '';
+        }
+    }
+    function ruleOfPassword(operate){
+        if(operate == 1){
+            emptyPassword();
+            document.getElementById('ruleOfPasswordObjuct').innerHTML = '密码可包含字母(区分大小写)、数字或英文标点符号，不能有空格等空白符，长度为6~14个字符';
+        }else if(operate == 0){
+            document.getElementById('ruleOfPasswordObjuct').innerHTML = '';
+        }
+    }
+    function ruleOfEmail(operate){
+        if(operate == 1){
+            document.getElementById('ruleOfEmailObjuct').innerHTML = '填写一个有效的电子邮箱地址，用于接收验证码';
+        }else if(operate == 0){
+            document.getElementById('ruleOfEmailObjuct').innerHTML = '';
+        }
+    }
 </script>
 
 <html>
-	<head>
-		<?php
-			if(isset($_POST['submitStatus'])){
-				if(strcasecmp($_SESSION['userTypedVerifCode'], $_SESSION['verifCode'])){
-					echo "<font size = '2' color = 'red'>验证码错误</font>";
-				}else if(!preg_match("/^[0-9a-zA-Z_]{1,32}$/" , $_SESSION['userTypedUserName'])){
-					echo "<font size = '2' color = 'red'>用户名不合法</font>";
-				}else if(!preg_match("/^[0-9a-zA-Z`~!@#$%^&*()\-_=+\[\{\]\}\|\\\;\:\'\"\,\<\.\>\/\?]{6,14}$/", $_SESSION['userTypedPassword'])){
-					echo "<font size = '2' color = 'red'>密码不合法</font>";
-				}else if(!preg_match("/^[a-z0-9](\.?[a-z0-9_\-]){0,}@[a-z0-9\-]+\.([a-z]{1,6}\.)*[a-z]{2,6}$/", $_SESSION['userTypedEmail'])){
-					echo "<font size = '2' color = 'red'>邮箱地址不合法</font>";
-				}else{
-					$tmp = $_SESSION['userTypedUserName'];
-					$result = mysql_query("SELECT userName FROM userList WHERE userName = \"$tmp\"", $_SESSION['conOfMysql']);
-					if($row = mysql_fetch_array($result)){
-						echo "<font size = '2' color = 'red'>用户名被占用</font>";
-					}else{
-						$tmp = $_SESSION['userTypedEmail'];
-						$result = mysql_query("SELECT email FROM userList WHERE email = \"$tmp\"", $_SESSION['conOfMysql']);
-						if($row = mysql_fetch_array($result)){
-							echo "<font size = '2' color = 'red'>该邮箱已经被注册</font>";
-						}else{
-							unset($_SESSION['signupTrying']);
-							$_SESSION['verifQuery'] = '1';
-							echo "<meta http-equiv = \"refresh\" content = \"0;url = http://123.206.204.23/nitmaker.cn/reborn/verification.php\">";
-						}
-					}
-				}
-			}
-		?>
-		<title>NITmaker-注册</title>
-	</head>
+    <head>
+<?php
+if(isset($_POST['submitStatus'])){
+    if(strcasecmp($_SESSION['userTypedVerifCode'], $_SESSION['verifCode'])){
+        echo "<font size = '2' color = 'red'>验证码错误</font>";
+    }else if(!preg_match("/^[0-9a-zA-Z_]{1,32}$/" , $_SESSION['userTypedUserName'])){
+        echo "<font size = '2' color = 'red'>用户名不合法</font>";
+    }else if(!preg_match("/^[0-9a-zA-Z`~!@#$%^&*()\-_=+\[\{\]\}\|\\\;\:\'\"\,\<\.\>\/\?]{6,14}$/", $_SESSION['userTypedPassword'])){
+        echo "<font size = '2' color = 'red'>密码不合法</font>";
+    }else if(!preg_match("/^[a-z0-9](\.?[a-z0-9_\-]){0,}@[a-z0-9\-]+\.([a-z]{1,6}\.)*[a-z]{2,6}$/", $_SESSION['userTypedEmail'])){
+        echo "<font size = '2' color = 'red'>邮箱地址不合法</font>";
+    }else{
+        $tmp = $_SESSION['userTypedUserName'];
+        $result = mysql_query("SELECT userName FROM userList WHERE userName = \"$tmp\"", $_SESSION['conOfMysql']);
+        if($row = mysql_fetch_array($result)){
+            echo "<font size = '2' color = 'red'>用户名被占用</font>";
+        }else{
+            $tmp = $_SESSION['userTypedEmail'];
+            $result = mysql_query("SELECT email FROM userList WHERE email = \"$tmp\"", $_SESSION['conOfMysql']);
+            if($row = mysql_fetch_array($result)){
+                echo "<font size = '2' color = 'red'>该邮箱已经被注册</font>";
+            }else{
+                unset($_SESSION['signupTrying']);
+                $_SESSION['verifQuery'] = '1';
+                unset($_POST['submitStatus']);
+                echo "<meta http-equiv = \"refresh\" content = \"0;url = http://123.206.204.23/nitmaker.cn/reborn/verification.php\">";
+            }
+        }
+    }
+}
+?>
+        <title>NITmaker-注册</title>
+    </head>
 
-	<body>
-		<h1>注册</h1>
-		<form method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
-			<table border = 0>
-				<tr> <th>用户名</th> <td><input type = 'text' id = 'usernameObjuct' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\'';?> onfocus = "ruleOfUserName(1)" onblur = "ruleOfUserName(0)"></td> <td id = 'ruleOfUserNameObjuct'></td> </tr>
-				<tr> <th>密码</th> <td><input type = 'password' id = 'passwordObjuct' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['modifiedUserTypedPassword'].'\'';?> onfocus = "ruleOfPassword(1)" onblur = "ruleOfPassword(0)"></td> <td id = 'ruleOfPasswordObjuct'></td> </tr>
-				<tr> <th>邮箱</th> <td><input type = 'text' id = 'emailObjuct' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\'';?> onfocus = "ruleOfEmail(1)" onblur = "ruleOfEmail(0)"></td> <td id = 'ruleOfEmailObjuct'></td> </tr>
-				<tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo '\''.$_SESSION['userTypedVerifCode'].'\'';?>></td> </tr>
-			</table>
-			<img title = '点击刷新' src = '../src/verification_class.php' align = 'absbottom' onclick = "this.src='../src/verification_class.php?'+Math.random();"></img>
-			<input type = 'hidden' name = 'submitStatus' value = '1'>
-			<input type = 'submit' value = '注册'>
-		</form>
-	</body>
+    <body>
+        <h1>注册</h1>
+        <form method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
+            <table border = 0>
+                <tr> <th>用户名</th> <td><input type = 'text' id = 'usernameObjuct' name = 'userTypedUserName' value = <?php echo  '\''.$_SESSION['userTypedUserName'].'\'';?> onfocus = "ruleOfUserName(1)" onblur = "ruleOfUserName(0)"></td> <td id = 'ruleOfUserNameObjuct'></td> </tr>
+                <tr> <th>密码</th> <td><input type = 'password' id = 'passwordObjuct' name = 'userTypedPassword' value = <?php echo '\''.$_SESSION['modifiedUserTypedPassword'].'\'';?> onfocus = "ruleOfPassword(1)" onblur = "ruleOfPassword(0)"></td> <td id = 'ruleOfPasswordObjuct'></td> </tr>
+                <tr> <th>邮箱</th> <td><input type = 'text' id = 'emailObjuct' name = 'userTypedEmail' value = <?php echo '\''.$_SESSION['userTypedEmail'].'\'';?> onfocus = "ruleOfEmail(1)" onblur = "ruleOfEmail(0)"></td> <td id = 'ruleOfEmailObjuct'></td> </tr>
+                <tr> <th>验证码</th> <td><input type = 'text' name = 'userTypedVerifCode' value = <?php echo '\''.$_SESSION['userTypedVerifCode'].'\'';?>></td> </tr>
+            </table>
+            <img title = '点击刷新' src = '../src/verification_class.php' align = 'absbottom' onclick = "this.src='../src/verification_class.php?'+Math.random();"></img>
+            <input type = 'hidden' name = 'submitStatus' value = '1'>
+            <input type = 'submit' value = '注册'>
+        </form>
+    </body>
 </html>
--- reborn/verification.php
+++ reborn/verification.php
@@ -1,38 +1,57 @@
 <?php
 session_start();
 
+if(isset($_POST['submitStatus'])){
+    if(strcasecmp($_POST['userTypedVerifCodeByEmail'], $_SESSION['verifCodeByEmail'])){
+        mysql_select_db("nitmaker_cn", $_SESSION['conOfMysql']);
+        $result = mysql_query("INSERT INTO userList (userName, password, email) VALUE (\"".$_SESSION['userTypedUserName']."\", \"".sha1($_SESSION['userTypedPassword'])."\", \"".$_SESSION['userTypedEmail']."\")");
+        if(mysql_affected_rows($result)){
+            $tmp = "注册成功";
+        }else{
+            $tmp = "数据库操作失败，请联系管理员17857021969";
+        }
+        echo "<html><head>";
+        echo "<meta http-equiv = \"refresh\" content = \"2;url = http://123.206.204.23/nitmaker.cn/reborn/login.php\">";
+        echo "</head><body>";
+        echo "<p><font size = '2' color = 'red'>".$tmp."</font></p>";
+        echo "<p><font size = '3' color = 'black'>2秒后自动转跳到<a href = 'http://123.206.204.23/nitmaker.cn/reborn/login.php'></a></font></p>";
+        echo "</body></html>";
+    }
+}
+
 if(!isset($_SESSION['verifQuery'])){
-	die("Permition Denied!");
+    die("Permition Denied!");
 }else{
-	unset($_SESSION['verifQuery']);
-	$to = $_SESSION['userTypedEmail'];
-	$subjuct = '欢迎注册NITmaker';
-	$_SESSION['verifCodeByEmail'] = bin2hex(openssl_random_pseudo_bytes(3));
-	$message = "<html>
-	<p><font size = '3' color = 'black'>亲爱的</font>\n<font size = '3' color = 'green'>".$_SESSION['userTypedUserName']."</font></p>\n
-	<p><font size = '3' color = 'black'>  您正在注册NITmaker，下面是您刚才\n注册时服务器向您发起的邮箱验证码：</font></p>\n
-	<p><font size = '6' color = 'blue'>\n".$_SESSION['verifCodeByEmail']."</font></p>\n
-	<p><font size = '3'>本站目前处于建设阶段，向注册用户提供SS服务的申请和\n查询服务。注册完成后请及时登录并填写基本信息。</font></p>\n
-	<p><font size = '3' color = 'red'>若非邮箱所有者您发起\n该验证，有扰您忽略本邮件。</font></p></html>
-	\n";
-	$addHeader = "From:NITmaker<nitmaker@163.com>\nMIME-Version:1.0\nContent-type:text/html\n";
-	mail($to, $subjuct, $message, $addHeader);
-    echo "<html>
-				<head>
-					<title>NITmaker-注册-邮箱验证</title>
-				</head>
-				<body>
-				<p><font size = '3' color = 'black'>服务器已经给您指定的邮箱</font></p>
-				<p><font size = '6' color = 'blue'>".$_SESSION['userTypedEmail']."</font></p>
-				<p><font size = '3' color = 'black'>发送了一封附有验证码的电子邮件。</font></p>
-				<p><font size = '3' color = 'black'>将邮件中的验证码填写到下面框中并点击\"验证\"按钮提交该验证码。</font></p>
-				<p><font size = '2' color = 'red'>请确保该邮箱归属于您，若要更改邮箱，请返回注册页面重新注册。</font></p>
-					<form method = 'post' action = ".htmlspecialchars($_SERVER['PHP_SELF']).">
-						<table border = 0>
-							<tr> <th>邮箱验证码</th> <td><input type = 'text' name = 'userTypedVerifCodeByEmail'></td> <td><input type = 'submit' value = '验证'></td> </tr>
-						</table>
-					</form>
-				<body>
-	</html>";
+    unset($_SESSION['verifQuery']);
+    $to = $_SESSION['userTypedEmail'];
+    $subjuct = '欢迎注册NITmaker';
+    $_SESSION['verifCodeByEmail'] = bin2hex(openssl_random_pseudo_bytes(3));
+    $message = "<html>
+        <p><font size = '3' color = 'black'>亲爱的</font>\n<font size = '3' color = 'green'>".$_SESSION['userTypedUserName']."</font></p>\n
+        <p><font size = '3' color = 'black'>  您正在注册NITmaker，下面是您刚才\n注册时服务器向您发起的邮箱验证码：</font></p>\n
+        <p><font size = '6' color = 'blue'>\n".$_SESSION['verifCodeByEmail']."</font></p>\n
+        <p><font size = '3'>本站目前处于建设阶段，向注册用户提供SS服务的申请和\n查询服务。注册完成后请及时登录并填写基本信息。</font></p>\n
+        <p><font size = '3' color = 'red'>若非邮箱所有者您发起\n该验证，有扰您忽略本邮件。</font></p></html>
+        \n";
+$addHeader = "From:NITmaker<nitmaker@163.com>\nMIME-Version:1.0\nContent-type:text/html\n";
+mail($to, $subjuct, $message, $addHeader);
+echo "<html>
+    <head>
+    <title>NITmaker-注册-邮箱验证</title>
+    </head>
+    <body>
+    <p><font size = '3' color = 'black'>服务器已经给您指定的邮箱</font></p>
+    <p><font size = '6' color = 'blue'>".$_SESSION['userTypedEmail']."</font></p>
+    <p><font size = '3' color = 'black'>发送了一封附有验证码的电子邮件。</font></p>
+    <p><font size = '3' color = 'black'>将邮件中的验证码填写到下面框中并点击\"验证\"按钮提交该验证码。</font></p>
+    <p><font size = '2' color = 'red'>请确保该邮箱归属于您，若要更改邮箱，请返回注册页面重新注册。</font></p>
+    <form method = 'post' action = ".htmlspecialchars($_SERVER['PHP_SELF']).">
+    <table border = 0>
+    <tr> <th>邮箱验证码</th> <td><input type = 'text' name = 'userTypedVerifCodeByEmail'></td> <td><input type = 'submit' value = '验证'></td> </tr>
+    </table>
+    <input type = 'hidden' name = 'submitStatus' value = '1'>
+    </form>
+    <body>
+    </html>";
 }
 ?>
