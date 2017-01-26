c089dcbf31ce509dfb39b26db14fe42cedff5233
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-01-26 12:03:21 +0800

    login pages

--- reborn/login.php
+++ reborn/login.php
@@ -13,8 +13,6 @@ mysql_select_db("nitmaker_cn", $_SESSION['conOfMysql']);
 <html>
     <head>
         <title>NITmaker-登陆</title>
-    </head>
-    <body>
 <?php
 
 if(isset($_POST['submitStatus'])){
@@ -30,6 +28,7 @@ if(isset($_POST['submitStatus'])){
             if(!strcmp($row['password'], sha1($_SESSION['userTypedPassword']))){
                 //用户密码匹配
                 //登陆成功
+                echo "<meta http-equiv = \"refresh\" content = \"0;url = http://123.206.204.23/nitmaker.cn/reborn/center.php\">";
                 echo "<p><font size = '2' color = 'red'>登陆成功，2秒后转跳到用户中心</font></p>";
             }else{
                 //用户名和密码不匹配
@@ -45,6 +44,8 @@ if(isset($_POST['submitStatus'])){
     }
 }
 ?>
+    </head>
+    <body>
         <h1>登陆</h1>
         <form method = 'post' action = '<?php echo htmlspecialchars($_SERVER[PHP_SELF]);?>'>
             <table border = 0>
